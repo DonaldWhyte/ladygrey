17e14ea6b9013774cef362976a3d1e1315081c1d
Donald Whyte <donaldwhyte0@gmail.com>
2015-07-05 17:12:15 +0100

    Defined test cases

--- test/index.js
+++ test/index.js
@@ -1,2 +1,57 @@
-// TODO
+var expect = require("chai").expect;
 
+var ladygrey = require("../index");
+
+describe("PromiseExpectation", function() {
+
+    it("is initialised correctly", function() {
+        // TODO
+        expect(true).to.be.false;
+    });
+
+    it("fails when promise is resolved instead of rejected", function() {
+        // TODO
+        expect(true).to.be.false;
+    });
+
+    it("fails when promise is rejected instead of resolved", function() {
+        // TODO
+        expect(true).to.be.false;
+    });
+
+    it("fails when wrong arguments are passed to resolve", function() {
+        // TODO
+        expect(true).to.be.false;
+    });
+
+    it("fails when wrong arguments are passed to reject", function() {
+        // TODO
+        expect(true).to.be.false;
+    });
+
+    it("fails when mock verification fails", function() {
+        // TODO
+        expect(true).to.be.false;
+    });
+
+    it("fails when settled promise verification fails", function() {
+        // TODO
+        expect(true).to.be.false;
+    });
+
+    it("passes when all expectations are met", function() {
+        // TODO
+        expect(true).to.be.false;
+    });
+
+    it("throws exception when invalid expected promise result is specified", function() {
+        // TODO
+        expect(true).to.be.false;
+    });
+
+    it("throws exception when executed more than one", function() {
+        // TODO
+        expect(true).to.be.false;
+    });
+
+});
